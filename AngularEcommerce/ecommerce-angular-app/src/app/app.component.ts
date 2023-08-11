7c6647d3d5041a1599d860b0e6b8276cd52fa55c
Fernando Bascope <fbascopeslz@gmail.com>
2023-08-10 23:44:36 -0400

    Section 3: Register Professional Angular Architecture - Firebase configuration in Angular

--- AngularEcommerce/ecommerce-angular-app/src/app/app.component.ts
+++ AngularEcommerce/ecommerce-angular-app/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'ecommerce-angular-app';
+
+  private firestore: Firestore = inject(Firestore);
+  users$: Observable<User[]>;
+
+  constructor() {
+    const usersCollection = collection(this.firestore, 'test');
+    this.users$ = collectionData(usersCollection) as Observable<User[]>;
+
+    this.users$.subscribe({
+      next(response) { 
+        console.log(response); 
+      },
+      error(err) { 
+        console.error('Error: ' + err); 
+      },
+      complete() { 
+        console.log('Completed'); 
+      }
+    });    
+  }  
+}
+
+export interface User {
+  name: string;
+  lastname: string;
 }
