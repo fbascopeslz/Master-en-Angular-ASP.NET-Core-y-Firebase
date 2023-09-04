7ba60fef2caf0b52071f303c98d2f9b6fd7629ef
Fernando Bascope <fbascopeslz@gmail.com>
2023-09-03 22:04:41 -0400

    Section 4: Defining Graphical Interfaces SASS CSS in Angular - Header creation in Angular

--- AngularEcommerce/ecommerce-angular-app/src/app/app.module.ts
+++ AngularEcommerce/ecommerce-angular-app/src/app/app.module.ts
@@ -10,9 +10,12 @@ import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { provideStorage,getStorage } from '@angular/fire/storage';
 
+import { HeaderComponent } from './components/header/header.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
